460494c7a02704a0ca215caf22d90c0390625071
gadjiev <gadzhiev@vbankcenter.ru>
2025-08-23 16:45:34 +0300

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://sdk.developer.deepar.ai/maven-android-repository/releases/")
+    }
+}
+
+rootProject.name = "Kingman"
+include(":app")
